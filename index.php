00761d2c580fd5d76b5dfcef0ea9e9eced05e6c2
SECXPLORERS <sarun.psx@gmail.com>
2025-04-08 00:23:51 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>SecXplorers</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/secxplorers/assets/css/style.css">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <script src="/secxplorers/assets/js/jquery.js"></script>
+    <script src="/secxplorers/assets/js/main.js"></script>
+</head>
+<body>
+    <div class="container">
+        <nav class="navbar">
+            <img src="assets/img/background/logo.png" class="logo" draggable="false">
+            <ul class="navbar-menu">
+                <li><a>Home</a></li>
+                <li><a>About</a></li>
+                <li><a>Services</a></li>
+                <li><a>Contact</a></li>
+                <li><a class="signin">Sign in</a></li>
+                <li><a class="signup">Sign up</a></li>
+            </ul>
+        </nav>
+        <div class="body-content">
+            <div class="box-img">
+                <h3>SecXplorers<br><h2>แพลตฟอร์มเถื่อนสมัครวันนี้ได้เล่นชาติหน้า<br><h1>ข้องใจโทร 061-827-65XX</h1></h2></h3>
+                <img src="assets/img/background/smoke.gif" class="smoke" draggable="false">
+                <img src="assets/img/background/bg3.png" class="bg" draggable="false">
+            </div>
+        </div>
+        <div class="footer">
+            <p class="copyright">&copy; 2025 SecXplorers Developer. All rights reserved.</p>
+        </div>
+    </div>
+</body>
+</html>
